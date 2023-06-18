c9a989e30cc0c2cf3749501adb19203853af4f65
higorlucas125 <higorlaf@hotmail.com>
2023-06-18 20:32:06 -0300

    Authetication in google and mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,33 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-
+import { connectToDB } from "@utils/database";
+// console.log({
+//     clientId: process.env.GOOGLE_ID,
+//     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+// })
 const handler = NextAuth({
     providers: [
         GoogleProvider ({
-            clientId: '',
-            clientSecret:'',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session ({session}){
 
     },
     async singIn({profile}){
+        try{
+            await connectToDB();
 
+            // Check if a user already exist
+
+            //if not, create a new user
+
+            return true;
+        }catch(error){
+            console.log(error);
+            return false;
+        }
     }
 })
 
